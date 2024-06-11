5ef7ad5d99e4fe5e35f7e69d942873589de5d1fc
Abu Quatada <abuquatada84@gmail.com>
2024-06-11 21:23:15 +0530

    Apply the Corsheaders setting in setting.py

--- Core/settings.py
+++ Core/settings.py
@@ -31,6 +31,7 @@ DEBUG = os.environ.get('DEBUG') == 'True'
 # DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS=True
 
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media' ) 
 MEDIA_URL = '/media/'
@@ -57,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
