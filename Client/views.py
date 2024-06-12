f0274df848efb83140ebcbc69d3121797d14d87e
MD MUSA ALAM <mdmusaalam98019@gmail.com>
2024-06-12 15:50:28 +0530

    Completed CRUD Operation

--- Client/views.py
+++ Client/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .models import * 
+from .serializer import *
+from rest_framework import viewsets
 
 # Create your views here.
+
+
+class Technology_optionViewSet(viewsets.ModelViewSet):
+    queryset = Technology_option.objects.all()
+    serializer_class = Technology_optionSarializer
+
+
+
+class TechnologyViewSet(viewsets.ModelViewSet):
+    queryset = Technology.objects.all()
+    serializer_class = TechnologySerializer
+
+
+
+class Payment_methodViewSet(viewsets.ModelViewSet):
+    queryset = Payment_method.objects.all()
+    serializer_class = Payment_methodSerializer
+
+
+
+class TaxViewSet(viewsets.ModelViewSet):
+    queryset = Tax.objects.all()
+    serializer_class = TaxSerializer
