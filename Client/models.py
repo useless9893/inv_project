a413c998f979bd7c16a7ac0dc8066b06595d1d4c
MD MUSA ALAM <mdmusaalam98019@gmail.com>
2024-06-12 15:03:28 +0530

    Model Create

--- Client/models.py
+++ Client/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Technology_option(models.Model):
+    option_id = models.AutoField(primary_key=True)
+    option = models.CharField(max_length=155)
+
+
+class Technology(models.Model):
+    tech_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    option_id = models.ForeignKey(Technology_option,on_delete=models.CASCADE,null=True)
+
+class Payment_method(models.Model):
+    payment_method_id = models.AutoField(primary_key=True)
+    payment_type = models.CharField(max_length=55)
+
+
+class Tax(models.Model):
+    tax_id = models.AutoField(primary_key=True)
+    tax_name = models.CharField(max_length=155)
+    rate = models.IntegerField()
+
--- Core/urls.py
+++ Core/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('auth_user/',include('Auth_user.urls')),
     # path('employee/',include('Employee.urls')),
     # path('vendor/',include('Vendors.urls')),
-    # path('client/',include('Client.urls'))
+    path('client/',include('Client.urls'))
 ] 
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
