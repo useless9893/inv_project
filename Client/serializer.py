15199b3db2b0e42b3c9e4e6c9cba16e6d2381db9
MD MUSA ALAM <mdmusaalam98019@gmail.com>
2024-06-14 23:36:55 +0530

    create serializer payment

--- Client/serializer.py
+++ Client/serializer.py
@@ -68,3 +68,11 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
+        
