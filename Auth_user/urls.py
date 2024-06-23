3dd9e514701fb9758eec4386fb066644e563452f
Abu Quatada <abuquatada84@gmail.com>
2024-06-23 12:33:40 +0530

    endpoints created  City State & Country

--- Auth_user/urls.py
+++ Auth_user/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path ,include
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('city', CityViewSet,basename='City')
+router.register('state', StateViewSet,basename='State')
+router.register('country', CountryViewSet,basename='Country')
 
 urlpatterns = [
+     path('',include(router.urls)),
      path('login/', LoginView.as_view()),
 ]
