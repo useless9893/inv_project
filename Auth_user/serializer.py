46ad8518ab81555ffd5178b84733577de7cf3824
Abu Quatada <abuquatada84@gmail.com>
2024-06-23 12:26:36 +0530

    created city state & country serializer

--- Auth_user/serializer.py
+++ Auth_user/serializer.py
@@ -11,3 +11,20 @@ class CoreUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CoreUser  
         fields = ["user_id",'user_name','first_name','last_name','email','contact']
+        
+        
+class CountrySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Country
+        fields = ['country_id','country_name']        
+class StateSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = State
+        fields = ['state_id','state_name']        
+class CitySerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = City
+        fields = ['city_id','city_name']        
