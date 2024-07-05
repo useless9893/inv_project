32a4ba22741675e97724ebe2304461547abad372
Mehtabalam04 <mehtabalamansari235@gmail.com>
2024-07-05 15:37:00 +0530

    permissions

--- Auth_user/permissions.py
+++ Auth_user/permissions.py
@@ -11,16 +11,16 @@ class IsClientOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.CoreUser.is_client == True:
             return True
-        return obj.CoreUser == request.CoreUser
+        return False
     
 
 class IsEmployeeOwner(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method == ['GET','POST']:
             return True
         return False
     
     def has_object_permission(self, request, view, obj):
         if request.CoreUser.is_employee == True:
-            return True
-        return obj.CoreUser == request.CoreUser
+            return True 
+        return False
