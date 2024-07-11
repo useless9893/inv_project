4c9491c50b848c04ba135d01b2f175969d22953f
Abu Quatada <abuquatada84@gmail.com>
2024-07-11 22:06:18 +0530

    static file added

--- Core/settings.py
+++ Core/settings.py
@@ -52,6 +52,10 @@ CSRF_TRUSTED_ORIGINS = ['https://ei5osmw4z482.share.zrok.io']
 MEDIA_ROOT = os.path.join(BASE_DIR ,'media' ) 
 MEDIA_URL = '/media/'
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 INSTALLED_APPS = [
     'jazzmin',
@@ -139,7 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
